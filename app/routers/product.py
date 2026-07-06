0ccc915d5d132778cdb274afa69e6ce44e5ad8e9
Jonas A. Saraiva <jonas.a.saraiva@accenture.com>
2026-07-06 11:00:49 -0300

    #fix

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,32 +1,30 @@
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
 from app import schemas
-from app.services import ProductService
+from app.services import get_product_service, ProductService
 from app.database import get_db
 
 router = APIRouter(prefix="/products", tags=["produtos"])
-session = Depends(get_db)
-product_service = ProductService()
 
 @router.get("/")
-def list_products(db: Session = session) -> list[schemas.ProductResponse]:
-    return product_service.list_products(db)
+def list_products(db: Session = Depends(get_db), service: ProductService = Depends(get_product_service)) -> list[schemas.ProductResponse]:
+    return service.list_products(db)
 
 @router.get("/{id}")
-def get_product(id: int, db: Session = session) -> schemas.ProductResponse:
-    return product_service.get_product(db, id)
+def get_product(id: int, db: Session = Depends(get_db), service: ProductService = Depends(get_product_service)) -> schemas.ProductResponse:
+    return service.get_product(db, id)
 
 
 @router.post("/", response_model=schemas.ProductResponse, status_code=status.HTTP_201_CREATED)
-def create_product(product: schemas.ProductCreate, db: Session = session) -> schemas.ProductResponse:
-    return product_service.create_product(db, product)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db), service: ProductService = Depends(get_product_service)) -> schemas.ProductResponse:
+    return service.create_product(db, product)
 
 
 @router.put("/{id}", response_model=schemas.ProductResponse, status_code=status.HTTP_200_OK)
-def update_product(id: int, product: schemas.ProductUpdate, db: Session = session) -> schemas.ProductResponse:
-    return product_service.update_product(db, id, product)
+def update_product(id: int, product: schemas.ProductUpdate, db: Session = Depends(get_db), service: ProductService = Depends(get_product_service)) -> schemas.ProductResponse:
+    return service.update_product(db, id, product)
 
 
 @router.delete("/{id}", status_code=status.HTTP_200_OK)
-def delete_product(id: int, db: Session = session):
-    return product_service.delete_product(db, id)
+def delete_product(id: int, db: Session = Depends(get_db), service: ProductService = Depends(get_product_service)):
+    return service.delete_product(db, id)
--- app/services/product.py
+++ app/services/product.py
@@ -3,3 +3,7 @@ from app.repositories import ProductRepository
 class ProductService(ProductRepository):
     pass
         
+
+
+def get_product_service() -> ProductService:
+    return ProductService(repository=ProductRepository())
