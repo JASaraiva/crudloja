03b61e4cc762b1eead635d55888b5209e5455902
Jonas A. Saraiva <jonas.a.saraiva@accenture.com>
2026-07-13 12:26:52 -0300

    #feat: refactoring

--- app/repositories/product.py
+++ app/repositories/product.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from app.models import Product
-from app.schemas import ProductCreate, ProductUpdate
 
 
 class ProductRepository:
@@ -10,25 +9,24 @@ class ProductRepository:
         self.db = db
 
 
-    def create_product(self, product: ProductCreate) -> Product:
-        db_product = Product(**product.model_dump())
+    def create(self, db_product: Product) -> Product:
         self.db.add(db_product)
         self.db.commit()
         self.db.refresh(db_product)
         return db_product
 
 
-    def get_product(self, product_id: int) -> Product | None:
+    def get(self, product_id: int) -> Product | None:
         stmt = select(Product).where(Product.id == product_id)
         return self.db.execute(stmt).scalar_one_or_none()
 
 
-    def list_products(self) -> list[Product]:
+    def list(self) -> list[Product]:
         stmt = select(Product)
         return self.db.execute(stmt).scalars().all()
 
 
-    def update_product(self, product_id: int, product_data: ProductUpdate) -> Product:
+    def update(self, product_id: int, product_data: ProductUpdate) -> Product:
         product = self.get_product(product_id)
 
         for key, value in product_data.model_dump().items():
@@ -38,7 +36,7 @@ class ProductRepository:
         return product
 
 
-    def delete_product(self, product_id: int) -> None:
+    def delete(self, product_id: int) -> None:
         product = self.get_product(product_id)
         self.db.delete(product)
         self.db.commit()
--- app/services/product.py
+++ app/services/product.py
@@ -1,8 +1,6 @@
 from app.repositories import ProductRepository
 from app.exceptions import ProductNotFoundException
 from app.models import Product
-from app.schemas import ProductCreate
-
 
 class ProductService():
     
@@ -10,7 +8,7 @@ class ProductService():
         self.repository = repository
 
 
-    def get_product(self, product_id: int) -> Product:
+    def get(self, product_id: int) -> Product:
 
         product = self.repository.get_product(product_id)
 
@@ -21,11 +19,13 @@ class ProductService():
     
 
     
-    def list_products(self) -> Product:
-        products = self.repository.list_products()
+    def list(self) -> Product:
+        products = self.repository.list()
         return products
     
 
-    def create_product(self, product_data: ProductCreate) -> Product: 
-        return self.repository.create_product(product_data)
+    def create(self, product_create: Product) -> Product: 
+        db_product = Product(**product_create.model_dump())
+
+        return self.repository.create(db_product)
         
